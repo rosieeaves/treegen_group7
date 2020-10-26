656f70ef366d8e53a548b9a51b2369530cc5af9a
Simon Lichtinger <56595507+simonlichtinger@users.noreply.github.com>
2020-10-26 14:31:14 +0000

    Add routerBase

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,14 @@
 import colors from 'vuetify/es5/util/colors'
 
+const routerBase =
+  process.env.DEPLOY_ENV === "GH_PAGES"
+    ? {
+        router: {
+          base: "/my-nuxt-app/"
+        }
+      }
+    : {};
+
 export default {
   // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
   ssr: false,
