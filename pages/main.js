33f961511fa7f345420cc82d0bdce0922d699141
Simon Lichtinger <simon.lichtinger@sjc.ox.ac.uk>
2020-10-27 15:58:18 +0000

    Some basic vue gui suggestion

---
+++ pages/main.js
@@ -0,0 +1,15 @@
+import Tree from "../components/tree"
+
+export default function buttonClick() {
+    let t = new Tree(
+        document.getElementById("type").value, 
+        document.getElementById("angle").value, 
+        document.getElementById("axiom").value, 
+        document.getElementById("iter").value
+    );
+    alert("The tree should now be drawn if the code were there!");
+    alert(document.getElementById("type").value+ 
+    document.getElementById("angle").value+ 
+    document.getElementById("axiom").value+ 
+    document.getElementById("iter").value);
+}
---
+++ pages/main.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-layout>
+    <v-flex class="text-left">
+        <div id="gui">
+          Model type:
+          <select name = "type" id = "type">
+            <option value = "deterministic" selected>deterministic</option>
+            <option value = "stochastic">stochastic</option>
+          </select>
+          <br>
+
+          Branching angle:
+          <input type="text" name="angle" id="angle" value="25" />
+          <br>
+          
+          Number of iterations:
+          <input type="text" name="iter" id="iter" value="5" />
+          <br>
+
+          Axiom:
+          <input type="text" name="axiom" id="axiom" value="F" />
+          <br>
+
+          <v-btn large outlined v-on:click="draw">Draw Tree!</v-btn>
+        </div>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+  import buttonClick from './main.js'
+
+  export default {
+    el: '#gui',
+    methods: {
+      draw: function (event) {
+        buttonClick();
+      }
+    }
+  }
+</script>
