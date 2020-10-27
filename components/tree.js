ef792f5448b3438eb79cba0e95544dd1b4124975
Rosie Eaves <rosie.eaves@univ.ox.ac.uk>
2020-10-27 11:50:24 +0000

    #1 Implemented stochastic method.

--- components/tree.js
+++ components/tree.js
@@ -31,33 +31,77 @@ export class Tree {
             * :param ruleOut:      String which should replace the character specified in ruleIn during the L-System string generation.
             * 
             **/
-        this.rules[this.rules.length] = {
-            in: ruleIn,
-            out: ruleOut
+
+        /* If there are no rules yet, create a new rule.*/
+        if (this.rules.length == 0) {
+            this.rules[this.rules.length] = {
+                in: ruleIn,
+                out: [ruleOut]
+            }
         }
+
+        /* If rules already exist, run through the existing rule in parameters to see if they match ruleIn. */
+        else {
+            let match = false;
+            for (let i=0; i<this.rules.length; i++) {
+                /* If there is a match, add the ruleOut to the existing rule. */
+                if (this.rules[i].in == ruleIn) {
+                    this.rules[i].out[this.rules[i].out.length] = ruleOut;
+                    match = true;
+                    break;
+                }
+            }
+            /* If there is no existing rule, create a new rule. */
+            if (match == false) {
+                this.rules[this.rules.length] = {
+                    in: ruleIn,
+                    out: [ruleOut]
+                }
+            }
+        }
+        
     }
 
-    generateDeterministic() {
+    generate() {
         /** Generates a string which represents a tree using the deterministic L-System method based on the rules and axiom of the tree.
             * 
-            * :returns:        String which is produced as the reulst of n iterations of the deterministic L-System method on the axiom. 
+            * :returns:        String which is produced as the result of n iterations of the deterministic L-System method on the axiom. 
             * 
             **/
         let string = this.axiom;
+
+        /* Run through number of iterations. */
         for (let x=0; x<this.n; x++) {
+
             let newString = '';
+
+            /* Run through characters in string. */
             for (let i=0; i<string.length; i++) {
+
                 let match = 0;
-                
+
+                /* Run through rules to see if any in parameters match the current string character. */
                 for (let j=0; j<this.rules.length; j++) {
 
+                    /* Match current string character to rule in parameter. */
                     if (this.rules[j].in == string[i]) {
                         match = 1;
-                        newString += this.rules[j].out;
+
+                        if (this.modelType == 'deterministic') {
+                            newString += this.rules[j].out[0];
+                        }
+
+                        else if (this.modelType == 'stochastic') {
+                            let indexRand = Math.floor(this.rules[j].out.length*Math.random());
+                            newString += this.rules[j].out[indexRand];
+                        }
+
                         break;
                     }
 
                 }
+
+                /* If there was no rule matching the current string character, keep the character the same. */
                 if (match == 0) {
                     newString += string[i];
                 }
