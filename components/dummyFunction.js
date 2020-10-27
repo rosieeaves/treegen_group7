e50218a606289a5a05aba708fa1dbfa2f0b14241
Andrew Kirby <andrew.kirby@trinity.ox.ac.uk>
2020-10-27 13:53:48 +0000

    #5 Create artefact in autodocs.yml

--- components/dummyFunction.js
+++ components/dummyFunction.js
@@ -1,5 +1,9 @@
-/* Simple sum function to test the CI testing procedure */
-
+/**
+ * Function which adds two integers together
+ * @constructor
+ * @param {int} a - The value of the first integer
+ * @param {int} b - The value of the second integer
+ */
 function sum(a, b) {
   return a + b
 }
