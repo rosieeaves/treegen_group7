4f6fb239254ab9507d66538fbc25035c2c73bf50
Simon Lichtinger <simon.lichtinger@sjc.ox.ac.uk>
2020-10-28 10:36:48 +0000

    Added and example canvas

--- pages/main.js
+++ pages/main.js
@@ -1,12 +1,22 @@
-import Tree from "../components/tree"
+//import Tree from "../components/tree"
 
-export default function buttonClick() {
-    let t = new Tree(
+export default function buttonClick(ctx) {
+    /*let t = new Tree(
         document.getElementById("type").value, 
         document.getElementById("angle").value, 
         document.getElementById("axiom").value, 
         document.getElementById("iter").value
-    );
+    );*/
+
+    // Make and example canvas drawing
+    ctx.beginPath();
+    ctx.strokeStyle = 'white';
+    ctx.lineWidth = 1;
+    ctx.moveTo(0, 0);
+    ctx.lineTo(100, 100);
+    ctx.stroke();
+    ctx.closePath();
+
     alert("The tree should now be drawn if the code were there!");
     alert(document.getElementById("type").value+ 
     document.getElementById("angle").value+ 
--- pages/main.vue
+++ pages/main.vue
@@ -23,19 +23,41 @@
 
           <v-btn large outlined v-on:click="draw">Draw Tree!</v-btn>
         </div>
+
+        <br>
+
+        <div id="app">
+          <h1>Tree</h1>
+          <canvas id="myCanvas" width="560" height="360" />
+        </div>
     </v-flex>
   </v-layout>
 </template>
 
+<style>
+#myCanvas {
+  border: 1px solid grey;
+}
+</style>
+
 <script>
   import buttonClick from './main.js'
 
+
   export default {
-    el: '#gui',
+    data: {
+      canvas: null,
+    },
     methods: {
       draw: function (event) {
-        buttonClick();
-      }
+        var c = document.getElementById("myCanvas");
+        this.canvas = c.getContext('2d');
+        buttonClick(this.canvas);
+      },
+    },
+    mounted() {
+      var c = document.getElementById("myCanvas");
+      this.canvas = c.getContext('2d');
     }
   }
 </script>
