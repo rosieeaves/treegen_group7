8da82ccecf630447d5ee227f71f3c6c9ae4d1dfb
Rosie Eaves <rosie.eaves@univ.ox.ac.uk>
2020-10-28 10:34:26 +0000

    Prettier main.js main.vue

--- pages/main.js
+++ pages/main.js
@@ -1,15 +1,17 @@
-import Tree from "../components/tree"
+import Tree from '../components/tree'
 
 export default function buttonClick() {
-    let t = new Tree(
-        document.getElementById("type").value, 
-        document.getElementById("angle").value, 
-        document.getElementById("axiom").value, 
-        document.getElementById("iter").value
-    );
-    alert("The tree should now be drawn if the code were there!");
-    alert(document.getElementById("type").value+ 
-    document.getElementById("angle").value+ 
-    document.getElementById("axiom").value+ 
-    document.getElementById("iter").value);
+  let t = new Tree(
+    document.getElementById('type').value,
+    document.getElementById('angle').value,
+    document.getElementById('axiom').value,
+    document.getElementById('iter').value
+  )
+  alert('The tree should now be drawn if the code were there!')
+  alert(
+    document.getElementById('type').value +
+      document.getElementById('angle').value +
+      document.getElementById('axiom').value +
+      document.getElementById('iter').value
+  )
 }
